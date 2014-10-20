17d2299242b13deb27ca9bfe9a8462a2ce185c7f
Webchild <nic@webchild.com.au>
2014-10-20 22:56:03 +1100

    Remaining
    Remaining

---
+++ Optus/test/views/graph.spec.js
@@ -0,0 +1,10 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('Graph View', function () {
+
+    beforeEach(function () {
+        this.GraphView = new Tests.Views.Graph();
+    });
+
+});
---
+++ Optus/test/views/graphcomp.spec.js
@@ -0,0 +1,10 @@
+/*global beforeEach, describe, it, assert, expect  */
+'use strict';
+
+describe('Graphcomp View', function () {
+
+    beforeEach(function () {
+        this.GraphcompView = new Tests.Views.Graphcomp();
+    });
+
+});
